8a6359b9d8a91f98789f5ade59cd5f1d2b88dda5
trithe <thoeun.rithe@gmail.com>
2023-08-29 16:36:24 +0700

    update

--- navbar.js
+++ navbar.js
@@ -1,28 +1,28 @@
 var navbarData = [
-  { id: 1, name: "Home", name_kh: "ទំព័រដើម", href: "../index.html#home" },
+  { id: 1, name: "Home", name_kh: "ទំព័រដើម", href: "./index.html#home" },
   {
     id: 2,
     name: "Popular",
     name_kh: "តំបន់ពេញនិយម",
-    href: "../index.html#popular",
+    href: "./index.html#popular",
   },
   {
     id: 3,
     name: "Ancient Temple",
     name_kh: "តំបន់ស្រស់បំព្រង",
-    href: "../index.html#ancient_temple",
+    href: "./index.html#ancient_temple",
   },
   {
     id: 4,
     name: "Natural",
     name_kh: "តំបន់ធម្មជាតិ",
-    href: "../index.html#natural",
+    href: "./index.html#natural",
   },
   {
     id: 5,
     name: "Reviewer",
     name_kh: "អ្នករុករកតំបន់ទេសចរណ៍",
-    href: "../index.html#reviewer",
+    href: "./index.html#reviewer",
   },
 ];
 $.each(navbarData, function (key, value) {
